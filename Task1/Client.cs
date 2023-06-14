0b431a2dfaa32e54f40d05cf585ae1f91db55caf
Alexander Rozhdestvenskiy <ppcrn97@gmail.com>
2023-06-14 20:27:18 +0300

    add class Client add ui to mainwindow

--- Task1/Client.cs
+++ Task1/Client.cs
@@ -1,32 +1,84 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Task1
 {
-    public class Pair<TKey, TValue>
+    public class Pair<TKey, TValue> : INotifyPropertyChanged
     {
-        public TKey Key { get; set; }
-        public TValue Value { get; set; }
+        private TKey _key;
+        private TValue _value;
+
+        public TKey AccountName { 
+            get 
+            { 
+                return _key;
+            }
+            set
+            {
+                _key = value;
+                OnPropertyChanged("AccountName");
+            }
+        }
+        public TValue Balance 
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                OnPropertyChanged("Balance");
+            }
+        }
 
         public Pair() { }
 
-        public Pair(TKey key, TValue val)
+        public Pair(TKey AccountName, TValue Balance)
         {
-            this.Key = key;
-            this.Value = val;
+            this.AccountName = AccountName;
+            this.Balance = Balance;
         }
+
+        public JObject Serialize()
+        {
+            JObject jObj = new JObject();
+            jObj["AccountName"] = AccountName.ToString();
+            jObj["Balance"] = Balance.ToString();
+            return jObj;
+        }
+
+        #region INotifyPropertyChanged inheritance 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #endregion
     }
 
     internal class Client
     {
         public string Name { get; }
         public ObservableCollection<Pair<string, int>> Account { get; set; }
+
+        public Client() { }
 
         public Client(string Name, string account)
         {
@@ -46,7 +98,7 @@ namespace Task1
         {
             foreach (Pair<string, int> pair in Account)
             {
-                if(pair.Key == account)
+                if(pair.AccountName == account)
                 {
                     return false;
                 }
@@ -64,7 +116,7 @@ namespace Task1
         {
             foreach(Pair<string, int> pair in Account)
             {
-                if (pair.Key == account)
+                if (pair.AccountName == account)
                 {
                     Account.Remove(pair);
                     return true;
@@ -98,7 +150,7 @@ namespace Task1
         {
             for(int i = 0; i < Account.Count; i++)
             {
-                if (Account[i].Key == account)
+                if (Account[i].AccountName == account)
                 {
                     return i;
                 }
@@ -121,11 +173,11 @@ namespace Task1
                 return false;
             if(ind2 == -1) 
                 return false;
-            if (Account[ind1].Value < value)
+            if (Account[ind1].Balance < value)
                 return false;
 
-            Account[ind1].Value -= value;
-            Account[ind2].Value += value;
+            Account[ind1].Balance -= value;
+            Account[ind2].Balance += value;
             return true;
         }
 
@@ -141,12 +193,74 @@ namespace Task1
             int ind = IndexOf(fromAcc);
             if (ind == -1)
                 return false;
-            if (Account[ind].Value < value)
+            if (Account[ind].Balance < value)
                 return false;
 
-            Account[ind].Value -= value;
-            toClient.Account[0].Value += value;
+            Account[ind].Balance -= value;
+            toClient.Account[0].Balance += value;
             return true;
+        }
+
+        public JObject Serialize()
+        {
+            JObject obj = new JObject();
+            obj["Name"] = Name;
+
+            JArray array = new JArray();
+
+            foreach (Pair<string, int> pair in Account)
+            {
+                array.Add(pair.Serialize());
+            }
+
+            obj["Account"] = array;
+
+            return obj;
+        }
+    }
+
+    internal class ClientList : ObservableCollection<Client>
+    {
+        public ClientList Deserialize(string path)
+        {
+            string str = "";
+
+            try
+            {
+                using (Stream stream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    StreamReader streamReader = new StreamReader(stream);
+                    str = streamReader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("File does not open");
+            }
+
+            if (str != "")
+                return JsonConvert.DeserializeObject<ClientList>(str);
+            else
+                return null;
+        }
+
+        public void Serialize(string path)
+        {
+            JArray array = new JArray();
+
+            foreach (Client client in this)
+                array.Add(client.Serialize());
+
+            string str = array.ToString();
+
+            try
+            {
+                File.WriteAllText(path, str);
+            }
+            catch
+            {
+                MessageBox.Show("File does not open");
+            }
         }
     }
 }
